573c7afc59a9421b8d45de0d5d8548c4009f4679
Saulo Cavalcante <saulocavalcante25@hotmail.com>
2021-07-03 20:32:06 -0300

    Adiciona validacoes a tela de login

--- js/script.js
+++ js/script.js
@@ -1,28 +1,42 @@
 /*---> Criar function Logar();*/
 
 function logar(){
-    /*... 
-        1°. Está chama validarUser() para fazer a validação
-            do campo, (input), de usuário/email que deve ter 6 caracteres.
-        2°. Está chama validarSenha() para fazer a validação
-            do campo, (input), de senha que deve ter 8 caracteres.
-        3°. Linkar a tela de exibição do conteúdo após as verificações.
-    ...*/
     
-    /*---> chamar a página listaAnuncio.html <---*/
-    window.location.href = "http://127.0.0.1:5500/listaAnuncios.html";
+    if(validar_login()){
+        window.location.href = "http://127.0.0.1:5500/listaAnuncios.html";
+    }
+
 };
-function validaUser(){
-    /*... 
-        1°. Faz a validação do campo de usuário que deve 
-            ter 6 caracteres.
-    ...*/
-};
-function validarSenha(){
-    /*... 
-        1°. Faz a validação da senha que deve ter
-            8 caracteres.
-    ...*/
+var users = [
+    {"usuario": "JoeJose", "senha": "vereador123"},
+    {"usuario": "SauloCav", "senha": "dinosaulo123"},
+    {"usuario": "JohnLacerda", "senha": "teletubbies123"},
+];
+
+function validar_login() {
+
+    let user = document.getElementById('user').value;
+    let senha = document.getElementById('senha').value;
+
+    if((user == "") || (senha == "")){
+        alert("Usuario e Senha Devem ser Informados! Tente Novamente");
+        return false;
+    }
+
+    for (var u in users) {
+        var us = users[u];
+        if ((us.usuario === user) && (us.senha === senha)) {
+            return true;
+        }
+        if ((us.usuario === user) && (us.senha != senha)) {
+            alert("Senha Incorreta! Tente Novamente");
+            return false;
+        }
+    }
+
+    alert("Usuario e Senha Nao Reconhecidos! Tente Novamente");
+    return false;
+
 };
 
 function lista(){
