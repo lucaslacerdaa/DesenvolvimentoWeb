392dce7df6646e5e81c7400c4545c348242af8a1
Joelson Carvalho <joemacedo08@gmail.com>
2021-08-05 09:05:27 -0300

    adicionando vue router

---
+++ MapCov/Front/src/router/index.js
@@ -0,0 +1,26 @@
+import { createRouter, createWebHistory} from 'vue-router'
+import Login from '../views/Login.vue'
+import Signup from '../views/Signup'
+
+const routes = [
+ 
+    {
+        path: '/login',
+        name: 'login',
+        component: Login
+    },
+
+    {
+        path: '/signup',
+        name: 'signup',
+        component: Signup
+    }
+
+]
+
+const router = createRouter({
+    history: createWebHistory(process.env.BASE_URL),
+    routes
+})
+
+export default router
